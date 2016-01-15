f49408b0e00f22759cc604a3175055ec47c484aa
Ryan Coburn <ryan.a.coburn@gmail.com>
2016-01-14 22:42:10 -0800

    Updated lamp script with github files

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -40,6 +40,9 @@ mysql -u root -e "SHOW DATABASES";
 
 # download starter content
 
+cd /vagrant
+sudo -u vagrant wget -q https://raw.githubusercontent.com/racoburn/vagrant/master/files/index.html
+sudo -u vagrant wget -q https://raw.githubusercontent.com/racoburn/vagrant/master/files/info.php
 
 service httpd restart
 
